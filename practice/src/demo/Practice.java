bacb76047bab6ebc6a1caf364fe671e154fddc8b
AmolSaundarya <123649795+AmolSaundarya@users.noreply.github.com>
2023-01-30 18:13:54 +0530

    revers number

---
+++ practice/src/demo/Practice.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		int n=123;
+		int a;
+		
+		while(n>0) {
+			a=n%10;
+			n=n/10;
+			System.out.print(a);
+		}
+	
+	}
+
+}
