2b243904f9e6990f44d7f22eb681f36706459853
AmolSaundarya <123649795+AmolSaundarya@users.noreply.github.com>
2023-01-30 20:50:26 +0530

    newFile

---
+++ newproject/src/newPackage/NewClass.java
@@ -0,0 +1,18 @@
+package newPackage;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		
+		int n=123;
+		int p;
+		
+		while(n>0) {
+			p=n%10;
+			n=n/10;
+			System.out.print(p);
+			
+		}
+	}
+
+}
